050df717a8e91616d1141bcb801057e2f1c974b1
Eurin-lumen <eurindelagoldcoast@gmail.com>
2021-12-10 21:44:04 +0100

    Réalisation de la page d'inscription et automatisation de la partie title

---
+++ includes/constants.php
@@ -0,0 +1,2 @@
+<?php
+define('WEBSITE_NAME', 'Boom Social Network');
---
+++ partials/_header.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="fr">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="Réalisation d'un réseau social">
+    <meta name="author" content="Eurin HASH">
+ 
+    <title>
+      <?= 
+      //Automatisation du titre de la page dans chaque partie
+      // Application de la constante pour le titre
+          isset($title)
+          ? $title .' - '. WEBSITE_NAME
+          : WEBSITE_NAME .'- Simple Rapide Efficace';
+      ?>
+       
+    </title>
+
+    <!-- NOS FICHIERS CSS-->
+    <!-- Latest compiled and minified CSS -->
+    <!-- <link rel="stylesheet" href="assets/css/bootstrap.min.css"> -->
+    
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/css/main.css">
+    <!-- Optional theme -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap-theme.min.css">
+
+    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+
+  <body>
+      <?php include('partials/_nav.php'); ?>
